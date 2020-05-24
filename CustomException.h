58c1a77cbcf2287c955e7b4757ef84b231d808cc
empty-works <pama.mark@gmail.com>
2020-05-24 21:43:39 +0200

    Implemented derived function for custom exception.

--- CustomException.h
+++ CustomException.h
@@ -1,9 +1,12 @@
 #ifndef CUSTOM_EXCEPTION_H_
 #define CUSTOM_EXCEPTION_H_
 
-class CustomException {
+class CustomException: std::exception {
 
-
+	public:
+		virtual const char * what() const throw() {
+			throw "Custom exception thrown.";	
+		}
 };
 
 #endif
